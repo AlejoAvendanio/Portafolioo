be64305338ecc9642bb7ad6160f7b1d0065c0348
Alejo Avendaño <alejoaven80@gmail.com>
2023-01-10 13:06:51 -0300

    css

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -6,9 +6,9 @@ import "./style.css"
 export default function About() {
   return (
     <div className='about'>
-        <h2 className="h2About">About me</h2>
+        <h2 style={{textDecoration:"underLine"}} className="h2About">About me</h2>
         <span>
-            <p>Soy un estudiante de Ingeniería en Sistemas de la universidad técnica nacional, recibido del bootcamp Soy Henry, con muchas ganas de afrontar nuevos desafíos e ingresar en el mundo laboral para poder proporcionar todos mis conocimientos y expandirlos aún más!
+            <p>Soy estudiante de Ingeniería en Sistemas de la universidad técnica nacional, recibido del bootcamp Soy Henry, con muchas ganas de afrontar nuevos desafíos e ingresar en el mundo laboral para poder proporcionar todos mis conocimientos y expandirlos aún más!
             </p>
         </span>
         <button id='set' className='resume'><a href="./resume.pdf" download >Resume</a> <FontAwesomeIcon className='arrow' icon={faArrowsDownToLine}/></button>
